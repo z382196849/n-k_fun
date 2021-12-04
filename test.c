afd9946ae58c565e2e537a2dd8cfd7c43d77437b
z382196829 <1936851315@qq.com>
2021-12-04 23:31:01 +0800

    第十八次提交

---
+++ test.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+int fun(int n, int k)
+{
+    if(k == 0)
+    {
+	return 1;
+    }
+    else
+    {
+	return n * fun(n, k-1);
+    }
+}
+
+int main()
+{
+    int n = 0;
+    int k = 0;
+    int ret = 0;
+    printf("请输入：n = ");
+    scanf("%d", &n);
+    printf("请输入：k = ");
+    scanf("%d", &k);
+    ret = fun(n, k);
+    printf("n^k = %d\n", ret);
+    system("pause");
+    return;
+}
